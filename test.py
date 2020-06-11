f57cb18da3aa69ffc963d1183f7c1ef25ab5e70b
Diastowo <diastowo@gmail.com>
2020-06-11 16:48:59 +0700

    signit

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from gpiozero import LED
 from time import sleep
+import signal
+import sys
 
 pinEnable = LED(13)
 pinDir = LED(19)
@@ -8,6 +10,13 @@ pinPulse = LED(21)
 pinEnable.off()
 pinDir.on()
 
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+	pinEnable.on()
+	pinDir.off()
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
 while True:
     pinPulse.on()
     print('hehe')
@@ -15,6 +24,3 @@ while True:
     pinPulse.off()
     print('haha')
     sleep(1/1000)
-
-pinEnable.on()
-pinDir.off()
